b3332906b58f56aaf86a08a62cdee47f9923f660
UmarBis <bis.umar@icloud.com>
2025-04-16 09:13:35 +0400

    Update cipher_t1.py

--- tsk1/cipher_t1.py
+++ tsk1/cipher_t1.py
@@ -1,4 +1,3 @@
-
 def encrypt(text: str, key: str) -> str:
     """
     Шифрует текст с помощью ключа
@@ -8,12 +7,25 @@ def encrypt(text: str, key: str) -> str:
     """
     return ''.join(key.get(char, char) for char in text.lower())
 
-def invert_key(key):
-    """Создаёт обратный ключ для дешифрования."""
+
+def invert_key(key: str) -> str:
+    """
+    Инверсия ключа
+    :param key: ключ
+    :return: обратный ключ
+    """
     return {v: k for k, v in key.items()}
-def decrypt(text, key):
-    """Расшифровывает текст, используя обратный ключ."""
+
+
+def decrypt(text: str, key: str) -> str:
+    """
+    Дешифрованный текст
+    :param text: зашифрованный текст
+    :param key: ключ
+    :return:
+    """
     inverted_key = invert_key(key)
     return ''.join(inverted_key.get(char, char) for char in text.lower())
+
 
 
