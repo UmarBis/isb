d11a3288339939466f54607a77a8fefedf63559a
UmarBis <bis.umar@icloud.com>
2025-04-16 09:23:21 +0400

    Update main_t2.py

--- tsk2/main_t2.py
+++ tsk2/main_t2.py
@@ -1,6 +1,6 @@
 from isb.lab_1.tsk2.cipher_t2 import comp_key, decrypt_text
-from isb.lab_1.tsk2.fileprocess_t2 import read_file, write_to_file, load_freq, argset
-
+from isb.lab_1.tsk2.fileprocess_t2 import write_to_file, load_freq, argset
+from isb.lab_1.tsk1.fileprocess_t1 import read_file
 def main() -> None:
     args = argset()
 
@@ -9,12 +9,10 @@ def main() -> None:
     encrypted_text = read_file(args.enc_text)
     decryption_key = comp_key(encrypted_text, rus_freq)
     decrypted_text = decrypt_text(encrypted_text, decryption_key)
-    try:
-        write_to_file(decrypted_text, args.output_textfile, decryption_key, args.output_keyfile)
-        print("Дешифрование завершено!")
-        print("Результаты сохранены в файлы...")
-    except Exception as e:
-        print("Произошла ошибка при сохранении/записи текста:", str(e))
+   
+    write_to_file(decryption_key, args.output_keyfile)
+    print("Дешифрование завершено!")
+    print("Результаты сохранены в файлы...")
 
 if __name__ == "__main__":
     main()
