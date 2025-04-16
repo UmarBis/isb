691209e82994dc2c82f2309e60be45a4fdfa4252
UmarBis <bis.umar@icloud.com>
2025-04-16 09:18:19 +0400

    Update main_t1.py

--- tsk1/main_t1.py
+++ tsk1/main_t1.py
@@ -10,13 +10,11 @@ def main() -> None:
 
     encrypted_text = encrypt(original_text, key)
 
-    try:
-        write_file(encrypted_text, args.output_file)
-        print("Исходный текст:", original_text)
-        print("Зашифрованный текст:", encrypted_text)
-        print("Запись/сохранение выполнено")
-    except Exception as e:
-        print("Ошибка при сохранении в файл:", str(e))
+    write_file(encrypted_text, args.output_file)
+    print("Исходный текст:", original_text)
+    print("Зашифрованный текст:", encrypted_text)
+    print("Запись/сохранение выполнено")
+
 
 if __name__ == "__main__":
     main()
