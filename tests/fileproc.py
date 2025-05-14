cc9dbaf6fd5e5b30b55b70e0894d8d3af71f93fa
UmarBis <bis.umar@icloud.com>
2025-05-14 08:57:53 +0400

    Update fileproc.py

--- tests/fileproc.py
+++ tests/fileproc.py
@@ -1,4 +1,5 @@
 import argparse
+import json 
 
 def argset():
     """
@@ -19,5 +20,17 @@ def read_from_file(filename: str) -> str:
     try:
         with open(filename, 'r', encoding='utf-8') as f:
             return f.read().strip()
+    except Exception as e:
+        print("Error..", str(e))
+
+def read_json(filename: str) -> str:
+    """
+    Чтение файла
+    :param filename: имя файла
+    :return: значения оттуда 
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
     except Exception as e:
         print("Error..", str(e))
