2741812f0130efc761e33fac0d773737f9127668
UmarBis <bis.umar@icloud.com>
2025-06-08 20:04:56 +0400

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -5,8 +5,8 @@ from fileproccesing import FileUtils
 
 class HybridCipher:
     def __init__(self):
-        self.symmetric = ChaCha20Cipher()
-
+        pass
+        
     def generate_keys(self, symmetric_key_path: str, public_key_path: str, private_key_path: str):
         """
         Генерация всех ключей
@@ -15,54 +15,55 @@ class HybridCipher:
         :param private_key_path: путь к приватному ключу
         :return: шифрование и сохранение симметричного ключа
         """
-        symmetric_key_data = self.symmetric.generate_key()
+        symmetric_cipher = ChaCha20Cipher()
+        symmetric_key_data = symmetric_cipher.generate_key()
 
-        rsa = RSACipher()
-        rsa.generate_keys()
-        rsa.save_keys(private_key_path, public_key_path)
+        private_key, public_key = RSACipher.generate_keys()
+        RSACipher.save_keys(private_key, private_key_path, public_key_path)
 
-        rsa.load_keys(None, public_key_path)
-        encrypted_sym_key = rsa.encrypt(symmetric_key_data)
-        FileUtils.write_bytes(symmetric_key_path, encrypted_sym_key)
+        encrypted_key = RSACipher.encrypt(public_key, symmetric_key_data)
+        FileUtils.write_bytes(symmetric_key_path, encrypted_key)
 
-    def encrypt_file(self, input_path: str, output_path: str, private_key_path: str,
-                     encrypted_sym_key_path: str):
+    def encrypt_file(self, input_path: str, output_path: str,
+                     private_key_path: str, encrypted_sym_key_path: str):
         """
         Шифрование файла
-        :param input_path: входные данные
-        :param output_path: выходные данные
-        :param private_key_path: приватный ключ
-        :param encrypted_sym_key_path: зашифрованный симметричный ключ
-        :return: шифр файла
+        :param input_path: путь к исходному файлу
+        :param output_path: путь к зашифрованному файлу
+        :param private_key_path: путь к приватному ключу
+        :param encrypted_sym_key_path: путь к зашифрованному симметричному ключу
+        :return: шифрование и сохранение файла
         """
-        rsa = RSACipher()
-        rsa.load_keys(private_key_path, None)
 
+        private_key = RSACipher.load_private_key(private_key_path)
         encrypted_sym_key = FileUtils.read_bytes(encrypted_sym_key_path)
-        symmetric_key_data = rsa.decrypt(encrypted_sym_key)
-        self.symmetric.set_key(symmetric_key_data)
+        symmetric_key_data = RSACipher.decrypt(private_key, encrypted_sym_key)
 
-        data = FileUtils.read_bytes(input_path)
-        encrypted_data = self.symmetric.encrypt(data)
-        FileUtils.write_bytes(output_path, encrypted_data)
+        symmetric_cipher = ChaCha20Cipher()
+        symmetric_cipher.set_key(symmetric_key_data)
 
-    def decrypt_file(self, input_path: str, output_path: str, private_key_path: str,
-                     encrypted_sym_key_path: str):
+        plaintext = FileUtils.read_bytes(input_path)
+        encrypted = symmetric_cipher.encrypt(plaintext)
+        FileUtils.write_bytes(output_path, encrypted)
+
+    def decrypt_file(self, input_path: str, output_path: str,
+                     private_key_path: str, encrypted_sym_key_path: str):
         """
         Дешифрование файла
-        :param input_path: входное
-        :param output_path: выходное
-        :param private_key_path: приватный ключ
-        :param encrypted_sym_key_path: зашифрованный симметричный ключ
-        :return: дешифр файла
+        :param input_path: путь к зашифрованному файлу
+        :param output_path: путь к расшифрованному файлу
+        :param private_key_path: путь к приватному ключу
+        :param encrypted_sym_key_path: путь к зашифрованному симметричному ключу
+        :return: восстановление исходного файла
         """
-        rsa = RSACipher()
-        rsa.load_keys(private_key_path, None)
 
+        private_key = RSACipher.load_private_key(private_key_path)
         encrypted_sym_key = FileUtils.read_bytes(encrypted_sym_key_path)
-        symmetric_key_data = rsa.decrypt(encrypted_sym_key)
-        self.symmetric.set_key(symmetric_key_data)
+        symmetric_key_data = RSACipher.decrypt(private_key, encrypted_sym_key)
+
+        symmetric_cipher = ChaCha20Cipher()
+        symmetric_cipher.set_key(symmetric_key_data)
 
         encrypted_data = FileUtils.read_bytes(input_path)
-        decrypted_data = self.symmetric.decrypt(encrypted_data)
+        decrypted_data = symmetric_cipher.decrypt(encrypted_data)
         FileUtils.write_bytes(output_path, decrypted_data)
